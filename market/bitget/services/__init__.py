e3c2f5cc9a543fb274a2aa25d572667591b360ce
Sawyer Ma <mmw-solutions@web.de>
2025-07-25 01:34:03 +0200

    23_07_2025_Test3

--- market/bitget/services/__init__.py
+++
@@ -1,8 +0,0 @@
-# market/bitget/services/__init__.py
-"""
-Bitget Services Module
-"""
-
-from .rest_api import RestAPIService, BitgetRestService
-
-__all__ = ['RestAPIService', 'BitgetRestService']
